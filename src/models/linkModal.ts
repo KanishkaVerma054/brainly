8d294e42ceb9c5ba526779373aae4bde2774bb08
Kanishka Verma <kanishkav774@gmail.com>
2024-11-18 12:24:33 +0530

    backend folder added

--- src/models/linkModal.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { model, Schema } from "mongoose";
-
-const LinkSchema = new Schema({
-    hash: String,
-    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true}
-})
-
-export const LinkModal = model("Link", LinkSchema)
--- src/models/userModal.ts
+++
@@ -1,9 +0,0 @@
-import { model, Schema } from "mongoose";
-
-const UserSchema = new Schema ({
-    email: {type: String, unique: true},
-    password: String,
-    // userName: {type: String, unique: true}
-})
-
-export const UserModel = model("User", UserSchema)
